0188640e977a93505116f70b79f1a8fc7fde7e8a
Emi Soft <emi.softdeveloper@gmail.com>
2018-09-03 13:24:18 +0300

    Init

---
+++ Emi.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Maksim Skliarov.
+ * Project: emi-cms
+ * Company: Emi-Soft <emi.softdeveloper@gmail.com>
+ * Date: 03/09/2018
+ */
+
+require dirname(dirname(__DIR__)).'/yiisoft/yii2/Yii.php';
+
+class Emi extends Yii
+{
+
+}
